d079ce0690584ad0894f0d74cdf94c98bfa373ba
student <student@DESKTOP-L3ET5DL>
2018-10-18 18:37:30 +0300

    init

---
+++ JsonFormatterDemo/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonFormatterDemo
+{
+    [Serializable]
+    //[DataContract]
+    public class Person : ISerializable
+    {
+        //[DataMember]
+        public string name;
+        //[DataMember]
+        public int age;
+
+        public Person() {}
+
+        private Person(SerializationInfo info, StreamingContext context) {
+            name = info.GetString("user_name");
+            age = info.GetInt32("user_age");
+        }
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context) {
+            info.AddValue("user_name", name);
+            info.AddValue("user_age", age);
+        }
+    }
+}
--- JsonFormatterDemo/Program.cs
+++ JsonFormatterDemo/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
 
 namespace JsonFormatterDemo
 {
@@ -10,6 +12,33 @@ namespace JsonFormatterDemo
     {
         static void Main(string[] args)
         {
+            Person p = new Person();
+            p.name = "John";
+            p.age = 42;
+
+            MemoryStream stream1 = new MemoryStream();
+            DataContractJsonSerializer ser =
+                new DataContractJsonSerializer(typeof(Person));
+
+            ser.WriteObject(stream1, p);
+
+            stream1.Position = 0;
+            StreamReader sr = new StreamReader(stream1);
+            Console.Write("JSON form of Person object: ");
+            Console.WriteLine(sr.ReadToEnd());
+
+            stream1.Position = 0;
+            sr = new StreamReader(stream1);
+            var json = sr.ReadToEnd();
+
+            /*stream1.Position = 0;
+            Person p2 = (Person)ser.ReadObject(stream1);
+            Console.WriteLine($"Deserialized back, got name={p2.name}, age={p2.age}");*/
+
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            Person p2 = ser.ReadObject(ms) as Person;
+            Console.WriteLine($"Deserialized back, got name={p2.name}, age={p2.age}");
+            ms.Close();
         }
     }
 }
